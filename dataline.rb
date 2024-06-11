63517db55484caf8f57d9fda56970b0276b39b77
Rami <rami.awar.ra@gmail.com>
2024-06-11 19:52:01 +0200

    feat: Bump to 1.0.3

--- dataline.rb
+++ dataline.rb
@@ -1,8 +1,8 @@
 class Dataline < Formula
   desc "App to chat with your data securely"
   homepage "https://github.com/RamiAwar/dataline"
-  url "https://github.com/RamiAwar/dataline/releases/download/v1.0.2-beta/dataline-macos-latest.tar.zip"
-  sha256 "6f361da2b0dacb1409eeced13758efa67bbd4ed986b558d06c84dabe6300d4e3"
+  url "https://github.com/RamiAwar/dataline/releases/download/v1.0.3/dataline-macos-latest.tar.zip"
+  sha256 "931006a4e02fc4ed687d0b2a885b61997d238998992cceb6457e9eea078abc26"
   license "GPL-3.0-only"
 
   def install
