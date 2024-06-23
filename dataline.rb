545fd47c02571eb0831550c6a09aa644cc1266d5
Rami Awar <RamiAwar@users.noreply.github.com>
2024-06-23 20:48:28 +0200

    Update dataline.rb

--- dataline.rb
+++ dataline.rb
@@ -1,8 +1,8 @@
 class Dataline < Formula
-  desc "AI-powered data exploration and visualization tool that puts privacy first"
+  desc "AI-driven platform to chat with your data"
   homepage "https://github.com/RamiAwar/dataline"
-  url "https://github.com/RamiAwar/dataline/releases/download/v1.0.5/dataline-macos-latest.tar.zip"
-  sha256 "d767145b2a16213125af6b7b795b7ba3608456c7e61b48863cfbbb012449d4df"
+  url "https://github.com/RamiAwar/dataline/releases/download/v1.0.6/dataline-macos-latest.tar.zip"
+  sha256 "e27cc082e2467fe6346d8cc9aea2e1facd0f11ebf1cc0b0828f569651cde9fcc"
   license "GPL-3.0-only"
 
   def install
