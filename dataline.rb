a5dde7a9a26160d85d0a41db9d4724983e202721
Rami <rami.awar.ra@gmail.com>
2024-06-30 22:56:50 +0200

    chore: bumo

--- dataline.rb
+++ dataline.rb
@@ -1,8 +1,8 @@
 class Dataline < Formula
-  desc "AI-driven platform to chat with your data"
+  desc "AI-powered data analysis and visualization tool that puts privacy first"
   homepage "https://github.com/RamiAwar/dataline"
-  url "https://github.com/RamiAwar/dataline/releases/download/v1.0.6/dataline-macos-latest.tar.zip"
-  sha256 "e27cc082e2467fe6346d8cc9aea2e1facd0f11ebf1cc0b0828f569651cde9fcc"
+  url "https://github.com/RamiAwar/dataline/releases/download/v1.0.7/dataline-macos-latest.tar.zip"
+  sha256 "0ea42a50b539a33a904850eac03654ef999c0029aa8b5e749236e9b7a1811a8a"
   license "GPL-3.0-only"
 
   def install
