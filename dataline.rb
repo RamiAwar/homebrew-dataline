d030dc2d1181af16ac683081dd8a0454bf028cc9
Rami <rami.awar.ra@gmail.com>
2024-10-25 18:45:12 +0200

    feat: bump version

--- dataline.rb
+++ dataline.rb
@@ -1,7 +1,7 @@
 class Dataline < Formula
   desc "AI-powered data analysis and visualization tool that puts privacy first"
   homepage "https://github.com/RamiAwar/dataline"
-  url "https://github.com/RamiAwar/dataline/releases/download/v1.0.14/dataline-macos-latest.tar.zip"
+  url "https://github.com/RamiAwar/dataline/releases/download/v1.1.0/dataline-macos-latest.tar.zip"
   sha256 "6abf5438cadc2d07482a9921432f8e147c4f2a3cb8b9a898e8ace69cb16fab72"
   license "GPL-3.0-only"
 
