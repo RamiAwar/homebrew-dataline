203538921a283b4ca050aacd7a94bb1a86e83d54
Rami <rami.awar.ra@gmail.com>
2025-05-30 07:46:40 +0200

    chore: Bumped version

--- dataline.rb
+++ dataline.rb
@@ -1,9 +1,17 @@
 class Dataline < Formula
   desc "AI-powered data analysis and visualization tool that puts privacy first"
   homepage "https://github.com/RamiAwar/dataline"
-  url "https://github.com/RamiAwar/dataline/releases/download/v1.1.4/dataline-macos-latest.tar.zip"
-  sha256 "c1896dfb161c3e0827bb3afefd3b0b6c1b1624674d86ce7a3d23a0f4b9e5097d"
   license "GPL-3.0-only"
+  
+  on_arm do
+    url "https://github.com/RamiAwar/dataline/releases/download/v1.2.0/dataline-macos-arm64.tar.zip"
+    sha256 "a01b74babb686997a658e9d87cb3aff269c7bfe4d4976390636b77e8f0f29015" # Replace with actual ARM64 checksum
+  end
+      
+  on_intel do
+    url "https://github.com/RamiAwar/dataline/releases/download/v1.2.0/dataline-macos-x86_64.tar.zip"
+    sha256 "6653f7bd63024525f7045698e18b843033033cddeacd53a7388d441dc36c553c" # Replace with actual x86_64 checksum
+  end
 
   def install
     bin.install "dataline"
